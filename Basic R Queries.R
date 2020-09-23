a769d43503736874bd19a80fb3f3bacfd787a51d
perlineanisha <71762267+perlineanisha@users.noreply.github.com>
2020-09-23 13:23:24 +0300

    Add files via upload

---
+++ Basic R Queries.R
@@ -0,0 +1,20 @@
+1+1
+N<-array(c(1:5))
+N
+#Data frame
+DF<-data.frame(ID=c(1:5), Name=c('ASH','ANI','Jef','Dav','Kat'), marks = c(50,100,60,80,100))
+DF
+DF$Name
+DF$ID[1]
+EX<-data.frame(x=c(1:3),y=c('a','b','c'))
+EX
+EX$x[1]
+EX[1,1]
+EX[1,2]
+EX[3,2]
+EX[1,1]
+EX[3,1]
+EX[3,2]
+EX[1,]
+EX[1,,3]
+EX[1,,6]
